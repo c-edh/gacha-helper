84aeb0fa40182ebff1a37f31ceced9073d7bdbc2
trisaraahtops <sarahdueltgen@gmail.com>
2021-11-11 21:39:59 -0800

    added UI changes

---
+++ app/src/main/java/edu/csustan/cs3810/gacha_helper/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.csustan.cs3810.gacha_helper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View v){
+        Intent intent = new Intent(this, login.class);
+        this.startActivity(intent);
+    }
+    //public void goToSignUp(View v){
+    //    Intent intent = new Intent(this, signup.class);
+    //    this.startActivity(intent);
+    //}
+
+}
---
+++ app/src/main/java/edu/csustan/cs3810/gacha_helper/login.java
@@ -0,0 +1,14 @@
+package edu.csustan.cs3810.gacha_helper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
