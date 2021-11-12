eea4d6242a562a7048b402d9454aa07552428a2e
Corey Edh <edhcorey@yahoo.com>
2021-11-12 08:56:16 -0800

    improved the login page

--- app/src/main/java/edu/csustan/cs3810/gacha_helper/login.java
+++ app/src/main/java/edu/csustan/cs3810/gacha_helper/login.java
@@ -1,14 +1,25 @@
 package edu.csustan.cs3810.gacha_helper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
+
+    EditText emailText;             //email text
+    EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailText = (EditText) findViewById(R.id.editTextEmail);
+        passwordText = (EditText) findViewById(R.id.editTextPassword);
+    }
+
+    public void loginUser(View view){
+        System.out.println("test");
     }
 }
