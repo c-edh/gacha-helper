7aaaeaaeaef4337d484b322c140b2e12ccf64af0
Corey Edh <edhcorey@yahoo.com>
2021-11-12 09:18:44 -0800

    login button now submits data to firebase, it fails but it still submits

--- app/src/main/java/edu/csustan/cs3810/gacha_helper/login.java
+++ app/src/main/java/edu/csustan/cs3810/gacha_helper/login.java
@@ -1,14 +1,25 @@
 package edu.csustan.cs3810.gacha_helper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
 
     EditText emailText;             //email text
     EditText passwordText;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +28,33 @@ public class login extends AppCompatActivity {
 
         emailText = (EditText) findViewById(R.id.editTextEmail);
         passwordText = (EditText) findViewById(R.id.editTextPassword);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void loginUser(View view){
         System.out.println("test");
+        String email = emailText.getText().toString().trim();
+        String password = passwordText.getText().toString().trim();
+
+        System.out.print(email);
+        System.out.print(password);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            System.out.println("User login");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                           System.out.println("User login failed");
+                        }
+                    }
+                });
     }
+
+
 }
