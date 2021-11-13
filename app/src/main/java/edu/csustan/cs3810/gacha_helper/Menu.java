5fc6e0538e1c396de66ee92cddd563c59a829ad8
Corey Edh <edhcorey@yahoo.com>
2021-11-13 11:55:36 -0800

    Made a navigation menu

---
+++ app/src/main/java/edu/csustan/cs3810/gacha_helper/Menu.java
@@ -0,0 +1,38 @@
+package edu.csustan.cs3810.gacha_helper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void goToFarmArtifact(View view){ //if user presses Farm Artifact Button
+
+    }
+
+    public void goToLevelup(View view){ //if user presses Level Up Button
+
+    }
+
+    public void goToRecommend(View view){ //if user presses Recommend Button
+
+    }
+
+    public void goToViewSavedBuilds(View view){ // if user presses View Saved Button
+
+    }
+
+    public void goToCreateABuild(View view){ // if user presses Create a Build Button
+
+
+    }
+
+
+}
