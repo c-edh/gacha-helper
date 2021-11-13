0d3e3098fc81ee9cf324a1d6383e4934c0ec5735
Corey Edh <edhcorey@yahoo.com>
2021-11-13 08:40:01 -0800

    change some code for login procedures.

--- app/src/main/java/edu/csustan/cs3810/gacha_helper/login.java
+++ app/src/main/java/edu/csustan/cs3810/gacha_helper/login.java
@@ -3,21 +3,21 @@ package edu.csustan.cs3810.gacha_helper;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
 
-    EditText emailText;             //email text
-    EditText passwordText;
+    EditText editTextEmail;             //email text
+    EditText editTextPassword;
 
     private FirebaseAuth mAuth;
 
@@ -26,34 +26,53 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        emailText = (EditText) findViewById(R.id.editTextEmail);
-        passwordText = (EditText) findViewById(R.id.editTextPassword);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
 
         mAuth = FirebaseAuth.getInstance();
     }
 
-    public void loginUser(View view){
-        System.out.println("test");
-        String email = emailText.getText().toString().trim();
-        String password = passwordText.getText().toString().trim();
+    public void loginButtonPressed(View view){
+        loginUser();
+    }
 
-        System.out.print(email);
-        System.out.print(password);
+    private void loginUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            System.out.println("User login");
-                        } else {
-                            // If sign in fails, display a message to the user.
-                           System.out.println("User login failed");
-                        }
-                    }
-                });
+        if (email.isEmpty()){ //checks if email is empty
+            editTextEmail.setError("Required to type in Email");
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){ //checks
+            editTextEmail.setError("Enter valid email address.");
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required");
+        }
+
+        if(password.length()<6){
+            editTextPassword.setError("Password is wrong");
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //user is login in
+                    System.out.println("User is login");
+                }
+                else{
+                    System.out.println("Failed login");
+                   // Toast.makeText(loginActivity.this, "Failed to login, wrong email or password", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 
 
