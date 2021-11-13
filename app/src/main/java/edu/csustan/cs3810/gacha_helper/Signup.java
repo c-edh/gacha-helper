6e308a748c91d7d286830320ed7cc814a8b65785
Corey Edh <edhcorey@yahoo.com>
2021-11-13 09:20:01 -0800

    added some signup, crashes the program when signup button is clicked

--- app/src/main/java/edu/csustan/cs3810/gacha_helper/Signup.java
+++ app/src/main/java/edu/csustan/cs3810/gacha_helper/Signup.java
@@ -1,14 +1,44 @@
 package edu.csustan.cs3810.gacha_helper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends AppCompatActivity {
+
+    EditText editTextEmail;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
     }
+
+    public void signupButtonPressed(View view){ //when the user presses the sign up button
+        signupUser();
+    }
+
+    private void signupUser(){
+        //user sign up procedures
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        System.out.println(email);
+        System.out.println(password);
+    }
+
 }
